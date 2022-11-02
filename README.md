0ec7eab8dd15a47757ecc50526228a2e545f8043
Thomas Bartlett <tbartlett@venturewell.org>
2022-11-02 10:05:57 -0400

    many updates to logic around Plist management and added CLI parameters

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # macOS-Update-Notifier
 Nudge like tool to prompt users to update. See https://github.com/macadmins/nudge for the inspiration
 
+# Arguments
+Arguments must follow the following format:
+1. REQUIRED: major OS such as 11, 12, or 13. This should be the OS you want to enforce updates for. If you want everyone on the latest version of 12 specify that and it will only look for updates to 12. 
+2. REQUIRED: Major OS update deadline. This will be used if it is detected that a major update is required. For example if you specify 13 in paremeter one you can specify the date everyone needs to be on 13 by. This will not effect non-major updates. 
+3. OPTIONAL: Set this to "debug" for testing purposes
+4. OPTIONAL: The OS you wish to imitate for testing purposes. Parameter 3 must be set
+
 # Workflow
 - Pulls down data from Apple website about latest version of macOS
 - Compares whether current computer is on latest version
--- macOS_Update_Prompt.py
+++ macOS_Update_Prompt.py
@@ -12,6 +12,22 @@ from Foundation import NSLog
 import sys
 from packaging.version import Version
 import plistlib
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Settings logs to file if not from Jamf, for Jamf its better to have them report back to Jamf
+if sys.argv[1] == '/':
+    logging.basicConfig(format='%(asctime)s - %(message)s')
+else:
+    logging.basicConfig(filename='/Library/management/update.log', format='%(asctime)s - %(message)s')
+    # add a rotating handler
+    logger = logging.getLogger("Rotating Log")
+    handler = RotatingFileHandler('/Library/management/update.log', maxBytes=1000000,
+                                    backupCount=5)
+    logger.addHandler(handler)
+
+# Global Variables
+Plist = "/Library/Management/update.plist"
 
 class DialogAlert:
     def __init__(self, message):
@@ -39,72 +55,86 @@ class DialogAlert:
         return exit_code
 
 def dialog_Check():
+    """Ensure Dialog is installed"""
     dialogPath="/usr/local/bin/dialog"
     if os.path.exists(dialogPath):
-        write_log("Swift Dialog Installed, proceeding")
+        logger.warning("Swift Dialog Installed, proceeding")
     else:
-        write_log("Swift Dialog not installed, exiting")
+        logger.warning("Swift Dialog not installed, exiting")
         sys.exit(1)
 
-def run_Check(diff):
-    update_Plist = "/Library/Management/update.plist"
-    today = datetime.date.today()
-    with open(update_Plist, 'rb') as fp:
-        pl = plistlib.load(fp)
-    lastRun = pl["last-run"]
-    #deadline = datetime.datetime.strptime(pl["deadline"], '%Y-%m-%d')
-    deadline = pl["deadline"]
-    days_Left = int((deadline.date() - today).days)
-    if days_Left > 23 and diff < 2:
-        write_log("Still in Update grace period... exiting... ")
-        return 1, days_Left
-    elif days_Left > 0 and diff < 2:
-        if lastRun.date() == today:
-            write_log("Already ran today... exiting...")
-            return 1
-        else:
-            write_log("In regular update time frame... displaying dialog...")
-            return 2, days_Left
+def read_Plist():
+    """return value of Plist if is exists"""
+    if os.path.exists(Plist):
+        with open(Plist, 'rb') as fp:
+            pl = plistlib.load(fp)
+        return pl
     else:
-        write_log("Deadline has passed... displaying dialog...")
-        return 3, days_Left
+        logger.warning("No Plist detected...")
+        return
 
-def run_Time(postingDate, current_OS):
-    # Set variables
-    update_Plist = "/Library/Management/update.plist"
+def update_Plist(today, current_OS, final_Date=0):
+    """Update Plist and create if necessary"""
     update_Dir = "/Library/Management"
-    # Get time data
-    now = datetime.datetime.now()
-    todayString = str(datetime.date.today())
-    final_Date = postingDate + datetime.timedelta(days=30)
-    # Check if plist exists
-    if not os.path.exists(update_Plist):
-        if not os.path.exists(update_Dir):
-            os.makedirs(update_Dir)
-        # Create Plist
-        properties = {
-           "create-time": todayString,
-           "last-run": now,
-           "current_OS": current_OS,
-           "deadline": final_Date
-        }
-        fileName=open(update_Plist,'wb')
-        plistlib.dump(properties, fileName)
-        fileName.close()
-    else:
-        # Update Plist
-        with open(update_Plist, 'rb') as data:
+    if os.path.exists(Plist):
+        with open(Plist, 'rb') as data:
             plist = plistlib.load(data)
-            plist.update({"last-run": now})
+            plist.update({"last-run": today})
             # Check if OS updated since last run
             if Version(plist["current_OS"]) < Version(current_OS):
                 plist.update({"current_OS": current_OS})
                 plist.update({"deadline": final_Date})
         # Write updates out
-        with open(update_Plist, 'wb') as file:
+        with open(Plist, 'wb') as file:
             plistlib.dump(plist, file)
+    else:
+        if not os.path.exists(update_Dir):
+            os.makedirs(update_Dir)
+        # Create Plist
+        properties = {
+           "create-time": today,
+           "last-run": today,
+           "current_OS": current_OS,
+           "deadline": final_Date
+        }
+        days_Left = int((final_Date.date() - today.date()).days)
+        with open(Plist, 'wb') as file:
+            plistlib.dump(properties, file)
 
-def update_Online():
+def run_Check(diff, postingDate, current_OS, update_Type, major_OS_Deadline):
+    """Check if a dialog needs to be displayed"""
+    today = datetime.datetime.today()
+    final_Date = postingDate + datetime.timedelta(days=30)
+    ran_Today = False
+    if read_Plist():
+        plist_Value = read_Plist()
+        lastRun = plist_Value["last-run"]
+        deadline = plist_Value["deadline"]
+        if lastRun.date() == today.date():
+            ran_Today = True
+    else:
+        lastRun = today
+        deadline = final_Date
+    if update_Type == "Major":
+        deadline = major_OS_Deadline
+    days_Left = int((deadline.date() - today.date()).days)
+    update_Plist(today, current_OS, deadline)
+    if days_Left > 23 and diff < 2:
+        logger.warning("Still in Update grace period... exiting... ")
+        return 1, days_Left
+    elif days_Left > 0 and diff < 2:
+        if ran_Today:
+            logger.warning("Already ran today... exiting...")
+            return 1, days_Left
+        else:
+            logger.warning("In regular update time frame... displaying dialog...")
+            return 2, days_Left
+    else:
+        logger.warning("Deadline has passed... displaying dialog...")
+        return 3, days_Left
+
+def get_Latest_Update():
+    """Download latest update info from Apple website"""
     # set to 4th param to work with Jamf
     if (sys.argv[1]) == '/':
         majorOS = str(sys.argv[4])
@@ -125,57 +155,54 @@ def update_Online():
     return macOS_Latest, posting_Date
 
 def update_Type(latest, current_OS):
+    """Check what type of update we are dealing with"""
     current = Version(current_OS)
     latest = Version(latest)
     diff = 0
     if current.major == latest.major:
-        write_log("Major version match")
+        logger.warning("Major version match")
         if current.minor == latest.minor:
-            write_log("features version match")
+            logger.warning("features version match")
             if current.micro == latest.micro:
-                write_log("Version processing error... exiting...")
+                logger.warning("Version processing error... exiting...")
                 exit
             else:
-                write_log("Minor version change detected")
+                logger.warning("Minor version change detected")
                 Update = "minor"
                 time = 20
                 diff = diff + latest.micro - current.micro
         else:
-            write_log("Feature version change detected")
+            logger.warning("Feature version change detected")
             Update = "feature"
             time = 40
             diff = diff + latest.minor - current.minor + latest.micro
     else:
-        write_log("Major version change detected")
+        logger.warning("Major version change detected")
         Update = "Major"
         time = 60
         diff = diff + latest.major - current.major
     type = {"type": Update, "time": time, "current": current_OS, "diff": diff}
-    write_log(f"Update type details: {type}")
+    logger.warning(f"Update type details: {type}")
     return type
 
-def write_log(text):
-    """logger for macOS Update"""
-    NSLog("[mdm-switch] " + str(text))
-
 def update_Check():
-    # check for updates
+    """Check if updates show as available locally"""
     update = ["softwareupdate", "-l"]
     for i in range(1, 4):
         try:
             test = subprocess.run(update, text=True, timeout=30, capture_output=True)
-            write_log(test.stderr)
+            logger.warning(test.stderr)
             if test.stderr == "No new software available.\n":
-                write_log("update available online but not locally... running kickstart")
+                logger.warning("update available online but not locally... running kickstart")
                 subprocess.run(["launchctl", "kickstart", "-k", "system/com.apple.softwareupdated"])
                 if i == 3:
-                    write_log("update not available locally")
+                    logger.warning("update not available locally")
                     return False
             else:
-                write_log("update available locally")
+                logger.warning("update available locally")
                 return True
         except subprocess.TimeoutExpired as e:
-            write_log(f"Timeout expired with error: {e}. Running kickstart...")
+            logger.warning(f"Timeout expired with error: {e}. Running kickstart...")
             subprocess.run(["launchctl", "kickstart", "-k", "system/com.apple.softwareupdated"])
 
 def build_Message(current, latest, release, type, time, left):
@@ -185,60 +212,62 @@ def build_Message(current, latest, release, type, time, left):
     return message
 
 def main():
-    # cleanup old file if it exists
-    dialog_command_file = "/var/tmp/dialog.log"
-    if os.path.exists(dialog_command_file):
-        os.remove(dialog_command_file)
-    # check if dialog is installed and latest
-    dialog_Check()
-    # if second argument is debug thent he third argument can be used to set which OS we are testing
+    # Get arguments
+    current_OS = platform.mac_ver()[0]
+    # Jamf sends first arg as /
     if (sys.argv[1]) == '/':
-        if sys.argv[5] == "debug":
-            current_OS = sys.argv[6]
-        else:
-            current_OS = platform.mac_ver()[0]
+        major_OS_Deadline = sys.argv[5]
+        if len(sys.argv) > 6:
+            if sys.argv[6] == "debug":
+                current_OS = sys.argv[7]
     else:
-        if sys.argv[2] == "debug":
-            current_OS = sys.argv[3]
-        else:
-            current_OS = platform.mac_ver()[0]
-    latest_info = update_Online()
+        major_OS_Deadline = sys.argv[2]
+        if len(sys.argv) > 3:
+            if sys.argv[3] == "debug":
+                current_OS = sys.argv[4]
+                
+    major_OS_Deadline = datetime.datetime.strptime(major_OS_Deadline, '%Y-%m-%d')
+    # get latest update info from Apple        
+    latest_info = get_Latest_Update()
     if Version(current_OS) < Version(latest_info[0]):
-        # Write to disk update info
-        run_Time(latest_info[1], current_OS)
-        # Convert to human readable format
+        # cleanup old dialog command file if it exists
+        dialog_command_file = "/var/tmp/dialog.log"
+        if os.path.exists(dialog_command_file):
+            os.remove(dialog_command_file)
+        # check if dialog is installed and latest
+        dialog_Check()
+        # Convert update release date to human readable format
         release = latest_info[1].strftime("%m/%d/%Y")
         # check whether update is minor, major, etc and how many updates behind we are
         type = update_Type(latest_info[0], current_OS)
         # check when update was run last and determine if it needs to be displayed
-        check = run_Check(type["diff"])
+        check = run_Check(type["diff"], latest_info[1], current_OS, type["type"], major_OS_Deadline)
         if check[0] == 1:
             return
         elif check[0] == 2:
             message = build_Message(current_OS, latest_info[0], release, type["type"], type["time"], check[1])
             mainDialog = DialogAlert(message)
             run_Main = mainDialog.alert(mainDialog.content_dict)
-            if run_Main == 2:
-                write_log("user deferred")
-            elif run_Main == 0:
-                write_log("user opened update page")
+            if run_Main.returncode == 2:
+                logger.warning("user deferred")
+            elif run_Main.returncode == 0:
+                logger.warning("user opened update page")
             else:
-                write_log(f"Dialog closed with exit code: {run_Main}")
+                logger.warning(f"Dialog closed with exit code: {run_Main}")
         else: 
-            write_log("Update is past deadline")
+            logger.warning("Update is past deadline")
             message = build_Message(current_OS, latest_info[0], release, type["type"], type["time"], 0)
             finalDialog = DialogAlert(message + "\n\n**Notice: You have passed the deadline and this message will persist until you install the update and reboot**")
             finalDialog.content_dict["ontop"] = 1
             finalDialog.content_dict.pop("button2text")
             run_Final = finalDialog.alert(finalDialog.content_dict)
-            if run_Final == 2:
-                write_log("user deferred")
-            elif run_Final == 0:
-                write_log("user opened update page")
+            if run_Final.returncode == 2:
+                logger.warning("user deferred")
+            elif run_Final.returncode == 0:
+                logger.warning("user opened update page")
             else:
-                write_log(f"Dialog closed with exit code: {run_Final}")
+                logger.warning(f"Dialog closed with exit code: {run_Final}")
     else:
-        write_log("No update needed... updating last run and exiting...")
-        run_Time(latest_info[1], current_OS)
+        logger.warning("No update needed... exiting...")
 
 main()
